d46a1d7ce96d472e12fe9885d8df3447dfb9e5b0
Giulia Menin <giulia.menin@gmail.com>
2023-07-06 22:39:47 +0900

    done it, with pain blood and tears

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "restaurants", to: "restaurants#index", as: "restaurants"
-  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  root "restaurants#index"
+  # get "restaurants", to: "restaurants#index", as: "restaurants"
+  # get "restaurants/new", to: "restaurants#new", as: "new_restaurant"
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  # post "restaurants", to: "restaurants#create"
+
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: "new_review"
+  # post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: "post_review"
+
+  resources :restaurants, only: [:new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
